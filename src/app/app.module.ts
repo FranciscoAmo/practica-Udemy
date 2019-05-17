b8f48e89780bd3666d07487a20e9db794586ad3a
Francisco Amo <famoredondo@hotmail.com>
2019-05-17 16:52:09 +0200

    Template listo sin rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BodyComponent } from './paginas/body/body.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+ 
   ],
   imports: [
     BrowserModule,
